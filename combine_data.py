a067430b22243758f2c86da8e6488806dc2a9661
WangZixin67 <wzx20010607@outlook.com>
2024-03-17 20:24:12 +0800

    feat: preprocess

---
+++ combine_data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
+def combine_data():
+    application = pd.read_csv("./data/application_record.csv")
+    credit = pd.read_csv("./data/credit_record.csv")
+    join_df = pd.merge(application, credit, on="ID", how='inner')
+    columns_to_replace = ["FLAG_OWN_CAR", "FLAG_OWN_REALTY", "FLAG_WORK_PHONE", "FLAG_MOBIL", "FLAG_PHONE", "FLAG_EMAIL"]
+    join_df[columns_to_replace] = join_df[columns_to_replace].replace({'Y': 1, 'N': 0})
+    indue_status = ["X", "C"]
+    risk = join_df["STATUS"].isin(indue_status)
+    risk_value = np.where(risk, 0, 1)
+    join_df['RISK'] = risk_value
+
+    unique_income_types = join_df["NAME_INCOME_TYPE"].unique()
+    unique_education_types = join_df["NAME_EDUCATION_TYPE"].unique()
+    unique_family_status = join_df["NAME_FAMILY_STATUS"].unique()
+    unique_housing_types = join_df["NAME_HOUSING_TYPE"].unique()
+
+    print("unique income types: ", unique_income_types)
+    print("unique education types: ", unique_education_types)
+    print("unique family status: ", unique_family_status)
+    print("unique housing types: ", unique_housing_types)
+
+    join_df.to_csv("./data/complete_credit_record.csv")
+
+
+def data_analysis():
+    credit_data = pd.read_csv("./data/complete_credit_record.csv")
+
+
+def preprocess_data():
+    columns_to_normalize = ["CNT_CHILDREN", "AMT_INCOME_TOTAL"]
+
+
+combine_data()
+
