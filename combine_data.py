565641c2aca4a7177278a1be0214b24fd29ea877
judowha <e0376925@u.nus.edu>
2024-03-24 11:50:58 +0800

    Tune pre-processing

--- combine_data.py
+++ combine_data.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.metrics import matthews_corrcoef, confusion_matrix
+from sklearn.metrics import matthews_corrcoef, confusion_matrix, accuracy_score
 from scipy.stats import pearsonr
 from sklearn.model_selection import train_test_split
 from pgmpy.models import BayesianNetwork, MarkovNetwork
-from pgmpy.estimators import HillClimbSearch, MaximumLikelihoodEstimator, BicScore, BayesianEstimator, PC
+from pgmpy.estimators import HillClimbSearch, MaximumLikelihoodEstimator, BicScore, BayesianEstimator, PC, MmhcEstimator, ExpectationMaximization
 from pgmpy.inference import VariableElimination, BeliefPropagation
 
 def combine_data():
@@ -14,7 +14,7 @@ def combine_data():
     join_df = pd.merge(application, credit, on="ID", how='inner')
     columns_to_replace = ["FLAG_OWN_CAR", "FLAG_OWN_REALTY", "FLAG_WORK_PHONE", "FLAG_MOBIL", "FLAG_PHONE", "FLAG_EMAIL"]
     join_df[columns_to_replace] = join_df[columns_to_replace].replace({'Y': 1, 'N': 0})
-    indue_status = ["X", "C", "0", "1", "2"]
+    indue_status = ["X", "C", "0", "1"]
     risk = join_df["STATUS"].isin(indue_status)
     risk_value = np.where(risk, 0, 1)
     join_df['RISK'] = risk_value
@@ -49,10 +49,14 @@ def data_preprocess():
         risk_0_rows = credit_data[credit_data['RISK'] == 0]
         duplicated_rows = risk_0_rows.sample(n=diff, replace=True)
         credit_data = pd.concat([credit_data, duplicated_rows], ignore_index=True)
+    if diff < 0:
+        risk_0_rows = credit_data[credit_data['RISK'] == 1]
+        duplicated_rows = risk_0_rows.sample(n=-diff, replace=True)
+        credit_data = pd.concat([credit_data, duplicated_rows], ignore_index=True)
 
-    columns_to_normalize = ["CNT_CHILDREN", "AMT_INCOME_TOTAL", "CNT_FAM_MEMBERS", "DAYS_EMPLOYED", "DAYS_BIRTH"]
-    scaler = StandardScaler()
-    credit_data[columns_to_normalize] = scaler.fit_transform(credit_data[columns_to_normalize])
+    columns_to_normalize = ["AMT_INCOME_TOTAL", "DAYS_EMPLOYED", "DAYS_BIRTH"]
+    # scaler = StandardScaler()
+    # credit_data[columns_to_normalize] = scaler.fit_transform(credit_data[columns_to_normalize])
     for col in columns_to_normalize:
         credit_data[col] = pd.qcut(credit_data[col], q=10, labels=False, duplicates='drop') 
 
@@ -88,7 +92,7 @@ def train_model():
     data_df = data_df.drop(columns=["ID", "FLAG_WORK_PHONE", "FLAG_MOBIL", "FLAG_PHONE", "FLAG_EMAIL"])
     X_train, X_test, y_train, y_test = train_test_split(data_df, data_df["RISK"], test_size=0.2, random_state=42)
     
-    hc = PC(X_train)
+    hc = MaximumLikelihoodEstimator(X_train)
     best_model = hc.estimate(scoring_method=BicScore(X_train))
     edges = list(best_model.edges())
     model = BayesianNetwork(edges)
@@ -106,16 +110,42 @@ def train_model():
     for i in test_dict:
         query_result = infer.query(variables=['RISK'], evidence=i)
         result.append(np.argmax(query_result.values))
-        # try:
-        #     query_result = infer.query(variables=['RISK'], evidence=i)
-        #     result.append(np.argmax(query_result.values))
-        # except Exception as e:
-        #     print(e)
-        #     exit(0)
     matrix = confusion_matrix(y_test, result, labels=[0, 1])
     print(str(matrix))
+
+def train_customized():
+    data_df = pd.read_csv("./dataset/oversampling_records.csv")
+    data_df = data_df.drop(columns=["ID", "FLAG_WORK_PHONE", "FLAG_MOBIL", "FLAG_PHONE", "FLAG_EMAIL"])
+    print(data_df["RISK"].unique())
+    X_train, X_test, y_train, y_test = train_test_split(data_df, data_df["RISK"], test_size=0.1, random_state=42)
+    risk_num = (X_train["RISK"] == 1).sum()
+    print(risk_num / len(X_train))
+    # used_features = ['DAYS_EMPLOYED', 'AMT_INCOME_TOTAL', 'NAME_INCOME_TYPE', "NAME_EDUCATION_TYPE", "FLAG_OWN_REALTY"]
+    used_features = [ "AMT_INCOME_TOTAL", "NAME_EDUCATION_TYPE", "FLAG_OWN_REALTY", "DAYS_EMPLOYED", "CNT_CHILDREN", 
+                     "NAME_HOUSING_TYPE", "FLAG_OWN_CAR"]
+    # model = BayesianNetwork([('DAYS_EMPLOYED', 'AMT_INCOME_TOTAL'), ('NAME_INCOME_TYPE', 'AMT_INCOME_TOTAL'),
+    #                          ('AMT_INCOME_TOTAL', 'RISK'), ("NAME_EDUCATION_TYPE", "RISK"), ("FLAG_OWN_REALTY", "RISK")])
+    # estimator = MaximumLikelihoodEstimator(model, X_train)
+    model = BayesianNetwork([('DAYS_EMPLOYED', 'RISK'), ('DAYS_EMPLOYED', 'AMT_INCOME_TOTAL'), ('AMT_INCOME_TOTAL', 'RISK'), 
+                             ("NAME_EDUCATION_TYPE", "RISK"),("FLAG_OWN_REALTY", "RISK"), ("CNT_CHILDREN", "RISK"), 
+                             ("NAME_HOUSING_TYPE", "RISK"), ("FLAG_OWN_CAR", "RISK")])
+    model.fit(X_train, estimator=MaximumLikelihoodEstimator)
+    predictions = model.predict(X_test[used_features])
+    print(predictions["RISK"].unique())
+    print(len(y_test))
+    print(len(predictions))
+    accuracy = accuracy_score(y_test, predictions["RISK"].to_numpy())
+
+    # Calculate confusion matrix
+    conf_matrix = confusion_matrix(y_test, predictions["RISK"].to_numpy())
+
+    print("Accuracy:", accuracy)
+    print("Confusion Matrix:")
+    print(conf_matrix)
+
 
 combine_data()
 data_preprocess()
 analyze_data()
-train_model()
+# train_model()
+train_customized()
